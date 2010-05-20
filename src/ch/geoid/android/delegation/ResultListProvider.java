535ebfe66e94311c10de5d96ebd52a46fa718d24
Samuel Benz <benz@geoid.ch>
2010-05-20 08:43:15 +0200

    put the code under GPL

--- src/ch/geoid/android/delegation/ResultListProvider.java
+++ src/ch/geoid/android/delegation/ResultListProvider.java
@@ -1,4 +1,22 @@
 package ch.geoid.android.delegation;
+/* 
+ * Copyright (c) 2010 Samuel Benz <benz@geoid.ch>
+ * 
+ * This file is part of DNSdroid.
+ *
+ * DNSdroid is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * DNSdroid is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with DNSdroid.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 import java.util.HashMap;
 

cf8d472d8776cef3da69456b5819859346ddb4d9
Samuel Benz <benz@geoid.ch>
2010-05-20 22:55:01 +0200

    remove transparent background on the about screen

--- src/ch/geoid/android/delegation/AboutActivity.java
+++ src/ch/geoid/android/delegation/AboutActivity.java
@@ -20,7 +20,6 @@ package ch.geoid.android.delegation;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.WindowManager;
 
 /**
  * Name: AboutActivity<br>
@@ -36,7 +35,6 @@ public class AboutActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
     	super.onCreate(savedInstanceState);
-    	getWindow().setFlags(WindowManager.LayoutParams.FLAG_BLUR_BEHIND,WindowManager.LayoutParams.FLAG_BLUR_BEHIND);
     	setContentView(R.layout.about);
     }
 }
